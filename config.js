fef7102cc9c37ae15d26993c9c8a7a5272255cb3
chuminhhieu1704-source <chuminhhieu1704@gmail.com>
2025-12-15 12:16:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Tha cho đó ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Híu sẽ trân trong lần này híu hứa luôn ',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'nhaaaaaaaaa <33333333',
+    mess: 'Mình biết mà 🥰. ',
+    messDesc: 'Nào về híu qua đón đi chơi nha .',
     btnAccept: 'Okiiiii lun <3',
     messLink: '' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 
